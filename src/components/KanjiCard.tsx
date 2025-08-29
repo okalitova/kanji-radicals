53b0f769fcdd13ef69151310c08d9f56880da00d
Olga Kalitova <nimloth693@gmail.com>
2025-08-29 14:42:27 +0200

    Show the radicals card back when radical is clicked

--- src/components/KanjiCard.tsx
+++ src/components/KanjiCard.tsx
@@ -1,36 +1,55 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./KanjiCard.module.css";
 import KanjiSVG from "./KanjiSVG";
 import KanjiBack from "./KanjiBack";
 import RadicalBack from "./RadicalBack";
 import { KanjiInfo } from "@/types/KanjiInfo";
 import { RadicalInfo } from "@/types/RadicalInfo";
+import { radicalDB } from "@/mock/radicals";
+
 
 type KanjiCardProps = {
   kanji: KanjiInfo,
-  radical: RadicalInfo,
 };
 
-export default function KanjiCard({kanji, radical}: KanjiCardProps) {
+export default function KanjiCard({kanji}: KanjiCardProps) {
   const svgSrc = `/kanji/${kanji.character}.svg`;
+  const connectedRadicals = radicalDB;
   const [flipped, setFlipped] = useState(false);
+  const [radical, setRadical] = useState<string>();
+
+  const onRadicalClicked = (radicalName: string) => {
+    setRadical(radicalName);
+    flipCard();
+  };
+
+  const flipCard = () => {
+    // Clean up to the initial state when flipping back.
+    if (flipped && radical) {
+      setRadical("");
+    }
+    setFlipped(!flipped);
+  }
 
   return (
     <div
       className={`${styles.card}`}
-      onClick={() => setFlipped(!flipped)}
+      onClick={flipCard}
     >
       <div className={`${styles.inner} ${flipped ? styles.flipped : ""}`}>
         {/* Front: Kanji SVG */}
         <div className={styles.front}>
-            <KanjiSVG svgSrc={svgSrc} />
+            <KanjiSVG svgSrc={svgSrc} onRadicalClick={onRadicalClicked}/>
         </div>
 
         {/* Back: Meaning */}
         <div className={styles.back}>
-          <RadicalBack radical={radical} />
+          {radical ? 
+            <RadicalBack radical={radicalDB[radical!]} /> :
+            <KanjiBack kanji={kanji}/>
+          }
         </div>
       </div>
     </div>
--- src/components/KanjiSVG.tsx
+++ src/components/KanjiSVG.tsx
@@ -1,14 +1,17 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import styles from "./KanjiSVG.module.css";
 
 type KanjiSVGProps = {
   svgSrc: string;
+  onRadicalClick: (radical: string) => void;
 };
 
-export default function KanjiSVG({svgSrc }: KanjiSVGProps) {
+export default function KanjiSVG({svgSrc, onRadicalClick}: KanjiSVGProps) {
   const [svgContent, setSvgContent] = useState<string | null>(null);
+  const [flipped, setFlipped] = useState(false);
+  const containerRef = useRef<HTMLDivElement | null>(null);;
 
   useEffect(() => {
     fetch(`${svgSrc}`)
@@ -21,17 +24,45 @@ export default function KanjiSVG({svgSrc }: KanjiSVGProps) {
       .catch((err) => console.error(err));
   }, [svgSrc]);
 
+  // Adding the listener for clicks on radicals.
+  useEffect(() => {
+    if (!containerRef.current) return;
+
+    // One needs to get the kanji first, cause kanji itself is a `g[kvg:element]`.
+    const kanji = containerRef.current.querySelector("g")?.querySelector("g");
+
+    // Every radical that is not a descendant of another radical.
+    const radicals = kanji?.querySelectorAll("g[kvg\\:element]:not(:scope g[kvg\\:element] g[kvg\\:element])")!;
+
+    // Stable click handler for adding/removal.
+    const handleClick = (event: Event) => {
+      const radicalName = (event.currentTarget as SVGElement).getAttribute("kvg:element");
+      if (radicalName) {
+        onRadicalClick?.(radicalName);
+      }
+    };
+
+    radicals.forEach((radical) => radical.addEventListener("click", handleClick));
+
+    return () => {
+      radicals.forEach((radical) => {
+        radical.removeEventListener("click", handleClick);
+      });
+    };
+  }, [svgContent, onRadicalClick]);
+
   return (
     svgContent ? (
         <div 
             className={styles.kanji}
+            ref={containerRef}
             dangerouslySetInnerHTML={{ __html: svgContent }} 
         />
     ) : ("Loading...")
   );
 }
 
-function toSVG(srcText: string, enabledStrokeOrder = true): string {
+function toSVG(srcText: string, enabledStrokeOrder = false): string {
     const parser = new DOMParser();
     const svgDoc = parser.parseFromString(srcText, "image/svg+xml");
     const svgElement = svgDoc.querySelector("svg")!;
